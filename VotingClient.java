206640a56d9b9f98429ffc55d03563e87bb80f7f
JulianaSCode <107593400+JulianaSCode@users.noreply.github.com>
2024-11-17 22:39:05 -0300

    Update VotingClient.java
    remoção de comentários em português

--- VotingClient.java
+++ VotingClient.java
@@ -14,23 +14,22 @@ public class VotingClient {
         try (ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
              ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
 
-            // Receber a pergunta e opções
+        
             String question = (String) inputStream.readObject();
             String[] options = (String[]) inputStream.readObject();
 
-            // Exibir a pergunta e opções de voto
+       
             System.out.println(question);
             for (int i = 0; i < options.length; i++) {
                 System.out.println((i + 1) + ". " + options[i]);
             }
 
-            // Obter o CPF
+           
             System.out.print("Digite seu CPF (apenas números): ");
             String cpf = scanner.nextLine();
             outputStream.writeObject(cpf);
             outputStream.flush();
 
-            // Validar CPF e voto
             String serverResponse = (String) inputStream.readObject();
             if (!serverResponse.equals("CPF inválido ou já votou. Tente novamente.")) {
                 System.out.print("Escolha seu voto (número da opção): ");
@@ -38,7 +37,6 @@ public class VotingClient {
                 outputStream.writeObject(options[voteOption - 1]);
                 outputStream.flush();
                 
-                // Receber confirmação
                 serverResponse = (String) inputStream.readObject();
                 System.out.println(serverResponse);
             } else {
