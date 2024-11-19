06e3ad35b788e9bcf43351d26d9719813b0778ca
Mary <81496671+Mary-Jane13@users.noreply.github.com>
2024-11-18 21:57:00 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Iniciar o servidor em uma thread separada
+        // Initiate server
         new Thread(() -> {
             VotingServer server = new VotingServer();
             server.start();
         }).start();
 
-        // Iniciar a interface gráfica
+        // Initiate graphical interface
         SwingUtilities.invokeLater(() -> {
             VotingApp app = new VotingApp();
             app.setVisible(true);
